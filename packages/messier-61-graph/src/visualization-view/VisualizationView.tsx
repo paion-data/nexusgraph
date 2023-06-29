36955e880aa971cf485d1688729bebddcadd255d
Jack <jack20220723@gmail.com>
2023-06-29 14:21:47 +0800

    添加 SonarCloud 代码分析徽章

--- packages/messier-61-graph/src/visualization-view/VisualizationView.tsx
+++ packages/messier-61-graph/src/visualization-view/VisualizationView.tsx
@@ -18,6 +18,7 @@ export default function Visualization(props: VisualizationProps): JSX.Element | 
   const [nodes, setNodes] = useState<NodeModel[]>([]);
   const [relationships, setRelationships] = useState<RelationshipModel[]>([]);
 
+
   useEffect(() => {
     setNodes(props.editorNodes);
     setRelationships(props.editorRelationships);
