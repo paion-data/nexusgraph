bca7f71afe2221465fddd09e6b2e651079346b48
Jack <jack20220723@gmail.com>
2023-07-06 19:58:04 +0800

    Enable sentry.io monitoring for app.nexusgraph.com

--- packages/nexusgraph-app/src/index.tsx
+++ packages/nexusgraph-app/src/index.tsx
@@ -8,8 +8,12 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { GlobalState } from "../../nexusgraph-graph/src/shared/globalState";
 import rootReducers from "../../nexusgraph-graph/src/shared/rootReducer";
 
+import * as Sentry from '@sentry/react'
+
 const reducer = combineReducers<GlobalState>({ ...(rootReducers as any) });
 export const store = configureStore({ reducer });
+
+setupSentry()
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
@@ -19,3 +23,18 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+export function setupSentry(): void {
+  Sentry.init({
+    dsn: "SENTRY_IO_DSN_INJECTED_BY_CI_CD",
+    integrations: [
+      new Sentry.BrowserTracing({
+        tracePropagationTargets: ["localhost", /^https:\/\/app\.nexusgraph\.com/],
+      }),
+      new Sentry.Replay(),
+    ],
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
