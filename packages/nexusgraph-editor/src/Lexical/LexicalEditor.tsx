88cc004b0f4aa5954fde3058422eb8210cd9e156
Jack <jack20220723@gmail.com>
2023-07-12 12:07:31 +0800

    Let lexical maintain onchange plugin

--- packages/nexusgraph-editor/src/Lexical/LexicalEditor.tsx
+++ packages/nexusgraph-editor/src/Lexical/LexicalEditor.tsx
@@ -8,13 +8,39 @@ import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
 import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 
-import OnChangePlugin from "./plugins/NexusgraphOnChangePlugin";
-
 import styles from "./LexicalEditor.module.css";
+import { RemoteNaturalLanguageProcessor } from "./processor/RemoteNaturalLanguageProcessor";
+import { EditorState } from "lexical";
+import { useDispatch } from "react-redux";
+import { UPDATE_GRAPH } from "../../../nexusgraph-graph/src/shared/editor/editorDuck";
+import parse from "./parser/RawTextParser";
 
 export default function LexicalEditor({ lexicalEditorConfig }: { lexicalEditorConfig: any }): JSX.Element {
+
+  const dispatch = useDispatch();
+  const naturalLanguageProcessor = new RemoteNaturalLanguageProcessor();
+
+  function Placeholder(): JSX.Element {
+    return <div className={styles["editor-placeholder"]}>Enter some plain text...</div>;
+  }
+  
+  const onChange = function (editorState: EditorState): void {
+    editorState.read(() => {
+      const jsonObject = JSON.parse(JSON.stringify(editorState));
+      const editorLines: string[] = parse(jsonObject);
+      if (editorLines.length > 0) {
+        naturalLanguageProcessor.entityExtraction(editorLines).then((graphEditorState) => {
+          dispatch({ type: UPDATE_GRAPH, payload: graphEditorState });
+        });
+      }
+    });
+  }
+
+
+
   return (
     <LexicalComposer initialConfig={lexicalEditorConfig}>
       <div className={styles["editor-container"]}>
@@ -25,12 +51,8 @@ export default function LexicalEditor({ lexicalEditorConfig }: { lexicalEditorCo
         />
         <HistoryPlugin />
         <AutoFocusPlugin />
-        <OnChangePlugin />
+        <OnChangePlugin onChange={onChange}/>
       </div>
     </LexicalComposer>
   );
-}
-
-function Placeholder(): JSX.Element {
-  return <div className={styles["editor-placeholder"]}>Enter some plain text...</div>;
 }
