b5d5cbfea1acf823871e13114e32f9e834bc4d64
Jack <jack20220723@gmail.com>
2023-07-17 08:05:23 +0800

    Fix build error

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,6 +1,7 @@
 /*
  * Copyright 2023 Paion Data. All rights reserved.
  */
+import React from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
