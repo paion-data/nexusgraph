93c754754084386ffbcaa597933cbc8abc4c87cc
Jack <jack20220723@gmail.com>
2023-07-19 21:31:32 +0800

    Example docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -39,9 +39,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/paion-data/nexusgraph/tree/master/docs/",
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
