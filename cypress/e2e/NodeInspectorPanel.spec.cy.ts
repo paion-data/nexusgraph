e6829be8c17525726a3b2e195dbe8e85d386fa34
Jack <jack20220723@gmail.com>
2023-07-21 12:05:34 +0800

    Style

--- cypress/e2e/NodeInspectorPanel.spec.cy.ts
+++ cypress/e2e/NodeInspectorPanel.spec.cy.ts
@@ -4,15 +4,11 @@ describe("Graph browser stats panel E2E tests", () => {
   it("Panel can reflect the INITIAL graph rendering stats", () => {
     cy.visit("http://localhost:8080/").wait(1000);
 
-    cy.intercept(
-      "POST",
-      "*/v1/data/entityExtraction",
-      {
-        fixture: "single-rdf-pair-graph.json"
-      }
-    );
+    cy.intercept("POST", "*/v1/data/entityExtraction", {
+      fixture: "single-rdf-pair-graph.json",
+    });
 
-    cy.get(".editor-paragraph").type("testText").wait(2000);
+    cy.get(".editor-paragraph").type("testText").wait(1000);
     cy.get('[data-testid="property-details-overview-node-label-*"]').should("have.text", "* (2)");
   });
 });
