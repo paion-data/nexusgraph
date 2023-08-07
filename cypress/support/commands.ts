368080e3c2a98bbde945ca2dd62da74fb6b1cb8d
gongminghui <xxykx1016@163.com>
2023-08-07 18:34:34 +0800

    test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,7 +1,6 @@
 // Copyright 2023 Paion Data. All rights reserved.
 Cypress.Commands.add("login", ({ username, password }) => {
   cy.intercept("GET", "http://localhost:8080/").as("userLogin");
-
   cy.origin(
     Cypress.env("logtoEndpointUrl").concat("/sign-in"),
     { args: { username, password } },
@@ -11,9 +10,8 @@ Cypress.Commands.add("login", ({ username, password }) => {
       cy.get('input[name="identifier"]').type(username);
       cy.get('button[type="submit"]').click();
       cy.get('input[name="password"]').type(password);
-      cy.get('button[type="submit"]').click().wait(10000);
-
-      cy.wait("@userLogin");
+      cy.get('button[type="submit"]').click();
     }
   );
+  cy.wait("@userLogin");
 });
