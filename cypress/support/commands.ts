5509b0163909b0385ee05425df353df7ba52e99d
gongminghui <xxykx1016@163.com>
2023-08-07 16:47:17 +0800

    test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,20 +1,16 @@
 // Copyright 2023 Paion Data. All rights reserved.
 Cypress.Commands.add("login", ({ username, password }) => {
-  cy.visit("http://localhost:8080/", { failOnStatusCode: false });
 
   cy.origin(
     Cypress.env("logtoEndpointUrl").concat("/sign-in"),
     { args: { username, password } },
     ({ username, password }) => {
-      cy.request({
-        url: "http://localhost:8080/",
-        followRedirect: false,
-      }).then((resp) => {
-        cy.get('input[name="identifier"]').type(username);
-        cy.get('button[type="submit"]').click();
-        cy.get('input[name="password"]').type(password);
-        cy.get('button[type="submit"]').click().wait(1000);
-      });
+      cy.visit("http://localhost:8080/", { failOnStatusCode: false });
+
+      cy.get('input[name="identifier"]').type(username);
+      cy.get('button[type="submit"]').click();
+      cy.get('input[name="password"]').type(password);
+      cy.get('button[type="submit"]').click().wait(1000);
     }
   );
 });
