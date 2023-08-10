2e161e470fbd0ecbfe080f995f8cfc80416142ff
Xxy1016 <125425805+Xxy1016@users.noreply.github.com>
2023-08-10 10:07:31 +0800

    implement login (#142)

--- cypress/e2e/nexusgraph.spec.cy.ts
+++ cypress/e2e/nexusgraph.spec.cy.ts
@@ -2,9 +2,17 @@
  * Copyright 2023 Paion Data. All rights reserved.
  */
 describe("nexusgraph basic test", () => {
-  it("Enter text in the editor and the corresponding node is generated in the graph", () => {
-    cy.visit("http://localhost:8080/", { failOnStatusCode: false }).wait(1000);
+  beforeEach(() => {
+    cy.login({ username: Cypress.env("username"), password: Cypress.env("password") }).wait(10000);
+  });
 
+  afterEach(() => {
+    cy.clearAllCookies();
+    cy.clearAllLocalStorage();
+    cy.clearAllSessionStorage();
+  });
+
+  it("Enter text in the editor and the corresponding node is generated in the graph", () => {
     cy.intercept("POST", "/v1/data/entityExtraction", { fixture: "getEditorData.json" }).as("getEditorData");
 
     cy.get(".editor-paragraph").type("China").wait(1000);
@@ -12,8 +20,6 @@ describe("nexusgraph basic test", () => {
   });
 
   it('Click the menu "Expand" button to expand the neighbor node', () => {
-    cy.visit("http://localhost:8080/", { failOnStatusCode: false });
-
     cy.intercept("POST", "/v1/data/entityExtraction", { fixture: "getEditorData.json" }).as("getEditorData");
 
     cy.get(".editor-paragraph").type("China").wait(1000);
--- packages/nexusgraph-app/src/App.tsx
+++ packages/nexusgraph-app/src/App.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { RemoteNaturalLanguageProcessor } from "../../nexusgraph-nlp/src/processor/RemoteNaturalLanguageProcessor";
 import { useDispatch, useSelector } from "react-redux";
 import { UPDATE_NLPDATA, GlobalState } from "../../nexusgraph-provider";
+import { useLogto } from "@logto/react";
 
 /**
  * The component that defines the entire nexus graph app.
@@ -16,6 +17,7 @@ export default function App(): JSX.Element {
   const naturalLanguageProcessor = new RemoteNaturalLanguageProcessor();
   const dispatch = useDispatch();
   const entityExtrationTexts: string[] = useSelector((state: GlobalState) => state.editorLine);
+  const { signIn, isAuthenticated } = useLogto();
 
   useEffect(() => {
     if (entityExtrationTexts.length > 0) {
@@ -25,20 +27,26 @@ export default function App(): JSX.Element {
     }
   }, [entityExtrationTexts, dispatch]);
 
-  return (
-    <AppWrapper>
-      <EditorWrapper>
-        <EditorGlassCover>
-          <IconWapper>
-            <img className="img" src={require("../public/app-logo.png")} alt="error" width={150} height={150} />
-          </IconWapper>
-          <EditorCaption className="h1">Nexus Graph</EditorCaption>
-          <Editor />
-        </EditorGlassCover>
-      </EditorWrapper>
-      <GraphBrowserWrapper>
-        <GraphBrowser />
-      </GraphBrowserWrapper>
-    </AppWrapper>
-  );
+  if (isAuthenticated) {
+    return (
+      <AppWrapper>
+        <EditorWrapper>
+          <EditorGlassCover>
+            <IconWapper>
+              <img className="img" src={require("../public/app-logo.png")} alt="error" width={150} height={150} />
+            </IconWapper>
+            <EditorCaption className="h1">Nexus Graph</EditorCaption>
+            <Editor />
+          </EditorGlassCover>
+        </EditorWrapper>
+        <GraphBrowserWrapper>
+          <GraphBrowser />
+        </GraphBrowserWrapper>
+      </AppWrapper>
+    );
+  }
+
+  signIn("http://localhost:8080/login");
+
+  return <></>;
 }
