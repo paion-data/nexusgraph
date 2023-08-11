abd3dba4d4fa6b258f9b7724eb1caded0ba425e0
活泼到令人头疼的胡桃 <jack20220723@gmail.com>
2023-08-11 21:54:22 +0800

    Implement dedicated nexusgraph server for dev and testing (#156)
    * Implement dedicated nexusgraph server for dev and testing
    
    * Self-review
    
    * Self-review
    
    * Style
    
    * Fix ESLint
    
    * Suppress all TS error during TypeDoc phase (because it is stupid!)
    
    * Install server deps

---
+++ packages/nexusgraph-server/src/server.ts
@@ -0,0 +1,45 @@
+// Copyright 2023 Paion Data. All rights reserved.
+import express from "express";
+
+import { LowSync } from "lowdb";
+import { JSONFileSync } from "lowdb/node";
+import asyncHandler from "express-async-handler";
+
+const app = express();
+app.use(express.json());
+
+type Node = {
+  id: string;
+  fields: { name: string };
+};
+
+type Link = {
+  source: string;
+  target: string;
+  fields: { label: string };
+};
+
+type Data = {
+  nodes: Node[];
+  links: Link[];
+};
+
+type Documents = {
+  documents: string[];
+};
+
+const adapter = new JSONFileSync<Data>("src/db.json");
+const db = new LowSync<Data>(adapter, { nodes: [], links: [] });
+db.read();
+
+app.post(
+  "/entityExtraction",
+  asyncHandler(async (req, res) => {
+    const documents = req.body as Documents;
+    res.send(db.data);
+  })
+);
+
+app.listen(3000, () => {
+  console.log("listening on port 3000"); // eslint-disable-line no-console
+});
