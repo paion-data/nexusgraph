95be90429d419800edd371a27596f81e5bfc15aa
fannifanni <135085401+fannifanni@users.noreply.github.com>
2023-08-15 13:44:08 +0800

    Add font-size and font-family button (#155)
    * add font-size font-family
    
    * add font-size font-family
    
    * optiomize font e2e test
    
    * add comment
    
    * change unit test
    
    * change comment

---
+++ cypress/e2e/font-family-dropdown.spec.cy.ts
@@ -0,0 +1,27 @@
+beforeEach(() => {
+  cy.login({ username: Cypress.env("username"), password: Cypress.env("password") }).wait(10000);
+  cy.intercept("POST", "/v1/data/entityExtraction", { fixture: "getEditorData.json" });
+  cy.get(".editor-paragraph").type("China");
+  cy.get('span[data-lexical-text = "true"]').type("{selectall}");
+
+  cy.get('[aria-label = "Formatting options for font family"]').find(".text").should("have.text", "Arial");
+});
+
+afterEach(() => {
+  cy.get('[aria-label = "Formatting options for font family"]').click();
+  cy.contains("Arial").click();
+  cy.get('[aria-label = "Formatting options for font family"]').find(".text").should("have.text", "Arial");
+  cy.get(".editor-paragraph").find("span").and("have.css", "font-family").should("include", "Arial");
+});
+
+describe("FontFamilyDropDown button E2E test", () => {
+  const fontFamily = ["Courier New", "Georgia", "Times New Roman", "Trebuchet MS", "Verdana"];
+  for (let i = 0; i < fontFamily.length; i++) {
+    it(`${fontFamily[i]} button has an effect in dropdown`, () => {
+      cy.get('[aria-label = "Formatting options for font family"]').click();
+      cy.contains(fontFamily[i]).click();
+      cy.get('[aria-label = "Formatting options for font family"]').find(".text").should("have.text", fontFamily[i]);
+      cy.get(".editor-paragraph").find("span").and("have.css", "font-family").should("include", fontFamily[i]);
+    });
+  }
+});
--- packages/nexusgraph-editor/src/Lexical/plugins/ToolbarPlugin.test.tsx
+++ packages/nexusgraph-editor/src/Lexical/plugins/ToolbarPlugin.test.tsx
@@ -55,14 +55,48 @@ test("OptionsButton text should be added", async () => {
   );
   const spanList = [
     "Normal",
-    "Large Heading",
-    "Small Heading",
+    "Heading 1",
+    "Heading 2",
+    "Heading 3",
+    "Heading 4",
+    "Heading 5",
+    "Heading 6",
     "Bulleted List",
     "Numbered List",
     "Quote",
     "Code Block",
   ];
-  fireEvent.click(await screen.findByRole("button", { name: /Formatting Options/i }));
+  fireEvent.click(await screen.findByRole("button", { name: "Formatting Options" }));
+  setTimeout(async () => {
+    for (let i = 0; i < spanList.length; i++) {
+      expect(await screen.findByText(spanList[i])).toBeInTheDocument();
+    }
+  }, 3000);
+});
+
+test("FontFamilyOptionsButton text should be added", async () => {
+  render(
+    <StoreWrapper>
+      <LexicalEditor />
+    </StoreWrapper>
+  );
+  const spanList = ["Arial", "Courier New", "Georgia", "Times New Roman", "Trebuchet MS", "Verdana"];
+  fireEvent.click(await screen.findByRole("button", { name: /Formatting Options for font family/i }));
+  setTimeout(async () => {
+    for (let i = 0; i < spanList.length; i++) {
+      expect(await screen.findByText(spanList[i])).toBeInTheDocument();
+    }
+  }, 3000);
+});
+
+test("FontSizeOptionsButton text should be added", async () => {
+  render(
+    <StoreWrapper>
+      <LexicalEditor />
+    </StoreWrapper>
+  );
+  const spanList = ["10px", "11px", "12px", "13px", "14px", "15px", "16px", "17px", "18px", "19px", "20px"];
+  fireEvent.click(await screen.findByRole("button", { name: /Formatting Options for font size/i }));
   setTimeout(async () => {
     for (let i = 0; i < spanList.length; i++) {
       expect(await screen.findByText(spanList[i])).toBeInTheDocument();
