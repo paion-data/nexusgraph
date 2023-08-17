5128a6dd79b633a7a106632e375c1c5c75df4299
Jack <jack20220723@gmail.com>
2023-08-17 14:25:51 +0800

    update

--- .github/scripts/dev-e2e-test-script.sh
+++ .github/scripts/dev-e2e-test-script.sh
@@ -2,8 +2,6 @@
 
 # Copyright 2023 Paion Data. All rights reserved.
 
-echo "SPEC_FILE_PATH=$SPEC_FILE_PATH"
-
 yarn start &
 yarn wait-on-dev
-yarn e2e -- --spec "$SPEC_FILE_PATH"
+yarn e2e --spec "$SPEC_FILE_PATH"
--- .github/scripts/prod-e2e-test-script.sh
+++ .github/scripts/prod-e2e-test-script.sh
@@ -6,4 +6,4 @@ yarn build
 npm install -g serve
 serve -s dist -l 8080 &
 yarn wait-on-prod
-yarn e2e -- --spec "$SPEC_FILE_PATH"
+yarn e2e --spec "$SPEC_FILE_PATH"
