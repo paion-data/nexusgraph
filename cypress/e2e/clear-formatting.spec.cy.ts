c3e36cff7e658e25a5b07c0a1835405443feb1f2
fannifanni <3544242744@qq.com>
2023-08-17 18:09:13 +0800

    change clear formatting e2e test

--- cypress/e2e/clear-formatting.spec.cy.ts
+++ cypress/e2e/clear-formatting.spec.cy.ts
@@ -6,31 +6,42 @@ beforeEach(() => {
   cy.get('span[data-lexical-text = "true"]').type("{selectall}");
 });
 
-it.skip(`Clear formatting button has an effect`, () => {
-  cy.get('[aria-label = "Formatting options for font family"]').click();
-  cy.contains("Verdana").click();
-
-  cy.get('[aria-label = "Formatting options for font size"]').click();
-  cy.contains("20px").click();
-
-  cy.get('[aria-label = "Format Bold"]').click();
-  cy.get('[aria-label = "Format Italics"]').click();
-  cy.get('[aria-label = "Format Underline"]').click();
-  cy.get('[aria-label = "Format Strikethrough"]').click();
-  cy.get('[aria-label = "insert Code"]').click();
-
-  cy.get('[aria-label = "Formatting text color"]').click();
-  cy.get(".color-picker-basic-color").find("button").first().click();
-
+afterEach(() => {
   cy.get('[aria-label = "Formatting options for additional text styles"]').click();
   cy.contains("Clear Formatting").click();
 
-  cy.get(".editor-paragraph").find("span").should("exist");
-  cy.get(".editor-paragraph").find("span").not("have.css", "font-family");
-  cy.get(".editor-paragraph").find("span").not("have.css", "font-size");
-  cy.get(".editor-paragraph").find("span").not("have.class", "font-semibold");
-  cy.get(".editor-paragraph").find("span").not("have.class", "italic");
-  cy.get(".editor-paragraph").find("span").not("have.class", "underlined-line-through");
-  cy.get(".editor-paragraph").find("span").not("have.class", "code");
-  cy.get(".editor-paragraph").find("span").not("have.css", "color");
+  cy.get(".editor-paragraph").find("span").not("have.style");
+  cy.get(".editor-paragraph").find("span").not("have.class");
+});
+
+describe("Clear formatting for font family, font size, font color, and font background color", () => {
+  it.skip("Clear formatting for font family", () => {
+    cy.get('[aria-label = "Formatting options for font family"]').click();
+    cy.contains("Verdana").click();
+  });
+
+  it.skip("Clear formatting for font size", () => {
+    cy.get('[aria-label = "Formatting options for font size"]').click();
+    cy.contains("20px").click();
+  });
+
+  it.skip("Clear formatting for font color", () => {
+    cy.get('[aria-label = "Formatting text color"]').click();
+    cy.get(".color-picker-basic-color").find("button").first().click();
+  });
+
+  it.skip("Clear formatting for font background color", () => {
+    cy.get('[aria-label = "Formatting background color"]').click();
+    cy.get(".color-picker-basic-color").find("button").first().click();
+  });
+});
+
+describe("Clear formatting for bold italic underline strikethrough and code", () => {
+  const formatting = ["Format Bold", "Format Italics", "Format Underline", "Format Strikethrough", "insert Code"];
+
+  for (let i = 0; i < formatting.length; i++) {
+    it.skip(`Clear formatting for ${formatting[i]}`, () => {
+      cy.get(`[aria-label = '${formatting[i]}']`).click();
+    });
+  }
 });
