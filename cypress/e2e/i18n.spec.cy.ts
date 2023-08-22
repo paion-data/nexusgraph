fc5ec432263138cd75015eff0058ebddd8e64aa0
gongminghui <xxykx1016@163.com>
2023-08-22 11:38:29 +0800

    Translate browser through i18n test

--- cypress/e2e/i18n.spec.cy.ts
+++ cypress/e2e/i18n.spec.cy.ts
@@ -3,5 +3,12 @@
  */
 it("Translate browser through i18n", () => {
   cy.login({ username: Cypress.env("username"), password: Cypress.env("password") }).wait(10000);
-  cy.get("div").should("contain", "概述");
+
+  const userLang = navigator.language;
+
+  if (userLang === "zh-CN") {
+    cy.get("div").should("contain", "概述");
+  } else {
+    cy.get("div").should("contain", "Overview");
+  }
 });
