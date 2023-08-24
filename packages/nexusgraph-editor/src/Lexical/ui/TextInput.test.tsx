b7181f33f89c7a8a5a0bff6681db33f361de8aca
Jack <jack20220723@gmail.com>
2023-08-24 17:22:01 +0800

    Add basic test demo

---
+++ packages/nexusgraph-editor/src/Lexical/ui/TextInput.test.tsx
@@ -0,0 +1,12 @@
+// Copyright 2023 Paion Data. All rights reserved.
+import { describe, expect } from '@jest/globals';
+import { render, screen } from '@testing-library/react';
+
+import TextInput from "./TextInput";
+
+describe('text input DOM test', () => {
+  test('Placeholder defaults to an empty string', () => {
+    render(<TextInput label='my input' value='text in input box' onChange={() => {}}/>);
+    expect(screen.getByPlaceholderText('')).not.toBeNull
+  });
+});
