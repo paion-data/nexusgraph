60da4fed8d8009e3974e434482a35ed38b210ea0
fannifanni <3544242744@qq.com>
2023-09-01 11:32:04 +0800

    Perfect test

--- packages/nexusgraph-editor/src/Lexical/ui/DropDown.test.tsx
+++ packages/nexusgraph-editor/src/Lexical/ui/DropDown.test.tsx
@@ -6,67 +6,84 @@ import DropDown, { DropDownItem } from "./DropDown";
 
 describe("DropDown DOM test", () => {
   test("DropDown displays specified disabled", () => {
-    render(<DropDown disabled={true} stopCloseOnClickSelf buttonClassName={""} children={undefined} />);
+    render(<DropDown disabled={true} buttonClassName={""} children={undefined} />);
     expect(screen.getByRole("button")).toHaveProperty("disabled");
   });
 
+  test("DropDown displays specified children", () => {
+    render(<DropDown buttonClassName={""} children={<div data-testid="children" />} />);
+    fireEvent.click(screen.getByRole("button"));
+    expect(screen.getByTestId("children")).not.toBeNull;
+  });
+
   test("DropDown displays specified buttonAriaLabel", () => {
-    render(
-      <DropDown
-        disabled={false}
-        stopCloseOnClickSelf
-        buttonAriaLabel={"button"}
-        buttonClassName={""}
-        children={undefined}
-      />
-    );
-    expect(screen.getByRole("button", { name: /button/i })).not.toBeNull;
+    render(<DropDown disabled={false} buttonAriaLabel={"menu"} buttonClassName={""} children={undefined} />);
+    expect(screen.getByRole("button", { name: /menu/i })).not.toBeNull;
   });
 
   test("DropDown displays specified buttonClassName", () => {
-    render(<DropDown disabled={false} stopCloseOnClickSelf buttonClassName={"button"} children={undefined} />);
+    render(<DropDown disabled={false} buttonClassName={"button"} children={undefined} />);
     expect(screen.getByRole("button")).toHaveProperty("className", "button");
   });
 
   test("DropDown displays specified buttonIconClassName", () => {
-    render(
-      <DropDown
-        disabled={false}
-        stopCloseOnClickSelf
-        buttonIconClassName={"icon"}
-        buttonClassName={""}
-        children={undefined}
-      />
-    );
+    render(<DropDown disabled={false} buttonIconClassName={"icon"} buttonClassName={""} children={undefined} />);
     expect(document.getElementsByClassName("icon")[0].getAttribute("class")).toBe("icon");
   });
 
   test("DropDown displays specified buttonLabel", () => {
-    render(
-      <DropDown
-        disabled={false}
-        stopCloseOnClickSelf
-        buttonIconClassName={""}
-        buttonLabel={"label"}
-        buttonClassName={""}
-        children={undefined}
-      />
-    );
+    render(<DropDown disabled={false} buttonLabel={"label"} buttonClassName={""} children={undefined} />);
     expect(screen.getByText("label")).not.toBeNull;
   });
 
-  test("Mock onClick function in DropDown and be called", () => {
-    const clickevent = jest.fn();
+  test("StopCloseOnClickSelf is true, click the basic-color button, and the drop-down box will not disappear", () => {
     render(
-      <DropDown buttonClassName={""}>
-        <DropDownItem children={undefined} className={""} onClick={() => clickevent()} />
-      </DropDown>
+      <DropDown
+        disabled={false}
+        stopCloseOnClickSelf={true}
+        buttonClassName={""}
+        children={<button data-testid="buttonitem" />}
+      />
     );
-    expect(clickevent).toHaveBeenCalled;
+    fireEvent.click(screen.getByRole("button"));
+    fireEvent.click(screen.getByTestId("buttonitem"));
+
+    expect(document.getElementsByClassName(".dropdown")).not.toBeNull;
+  });
+
+  test("StopCloseOnClickSelf is false, click the basic-color button, and the drop-down box will disappear", () => {
+    render(
+      <DropDown
+        disabled={false}
+        stopCloseOnClickSelf={false}
+        buttonClassName={""}
+        children={<button data-testid="buttonitem" />}
+      />
+    );
+    fireEvent.click(screen.getByRole("button"));
+    fireEvent.click(screen.getByTestId("buttonitem"));
+
+    expect(document.getElementsByClassName(".dropdown")).toBeNull;
   });
 });
 
 describe("DropDownItem DOM test", () => {
+  test("DropDownItem displays specified children", () => {
+    render(
+      <DropDown buttonClassName={""}>
+        <DropDownItem
+          children={<div data-testid="children" />}
+          className={""}
+          onClick={function (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+      </DropDown>
+    );
+    fireEvent.click(screen.getByRole("button"));
+    expect(screen.getByTestId("children")).not.toBeNull;
+  });
+
   test("DropDownItem displays specified title", () => {
     render(
       <DropDown buttonClassName={""}>
