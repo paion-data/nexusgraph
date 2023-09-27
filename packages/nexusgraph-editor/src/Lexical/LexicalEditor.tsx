c42761a62165e14a396d40ee4cd85534b66b198c
Jack <jack20220723@gmail.com>
2023-09-27 12:21:35 +0800

    Change import namespace

--- packages/nexusgraph-editor/src/Lexical/LexicalEditor.tsx
+++ packages/nexusgraph-editor/src/Lexical/LexicalEditor.tsx
@@ -1,76 +1,49 @@
 // Copyright 2023 Paion Data. All rights reserved.
-import { CodeHighlightNode, CodeNode } from "@paiondata/lexical-code";
-import { AutoLinkNode, LinkNode } from "@paiondata/lexical-link";
-import { ListItemNode, ListNode } from "@paiondata/lexical-list";
-import { AutoFocusPlugin } from "@paiondata/lexical-react/LexicalAutoFocusPlugin";
-import { CheckListPlugin } from "@paiondata/lexical-react/LexicalCheckListPlugin";
-import { InitialConfigType, LexicalComposer } from "@paiondata/lexical-react/LexicalComposer";
-import { ContentEditable } from "@paiondata/lexical-react/LexicalContentEditable";
-import LexicalErrorBoundary from "@paiondata/lexical-react/LexicalErrorBoundary";
-import { HistoryPlugin } from "@paiondata/lexical-react/LexicalHistoryPlugin";
-import { LinkPlugin } from "@paiondata/lexical-react/LexicalLinkPlugin";
-import { ListPlugin } from "@paiondata/lexical-react/LexicalListPlugin";
-import { RichTextPlugin } from "@paiondata/lexical-react/LexicalRichTextPlugin";
-import { HeadingNode, QuoteNode } from "@paiondata/lexical-rich-text";
-import { TableCellNode, TableNode, TableRowNode } from "@paiondata/lexical-table";
+import {LexicalComposer} from '@paiondata/lexical-react/LexicalComposer';
+import {SharedHistoryContext} from '@paiondata/lexical-playground/context/SharedHistoryContext';
+import { TableContext } from '@paiondata/lexical-playground/plugins/TablePlugin'
+import { SharedAutocompleteContext } from '@paiondata/lexical-playground/context/SharedAutocompleteContext'
+import { default as EditorNodes } from '@paiondata/lexical-playground/nodes/PlaygroundNodes'
+import { default as EditorTheme } from '@paiondata/lexical-playground/themes/PlaygroundEditorTheme'
+import Editor from '@paiondata/lexical-playground/Editor'
+import Settings from '@paiondata/lexical-playground/Settings'
 
-import styles from "./LexicalEditor.module.css";
-import CodeHighlightPlugin from "./plugins/CodeHighlightPlugin";
-import EditorContentUpdatePlugin from "./plugins/EditorContentUpdatePlugin";
-import OnChangePlugin from "./plugins/NexusgraphOnChangePlugin";
-import ToolbarPlugin from "./plugins/ToolbarPlugin";
-import { Paragraph } from "./plugins/ToolbarPlugin/styled";
-import ExampleTheme from "./themes/ExampleTheme";
-
+/**
+ * {@link LexicalEditor} does not initialize editor contents.
+ * @returns  
+ */
 export default function LexicalEditor(): JSX.Element {
-  const editorConfig: InitialConfigType = {
-    // The editor theme
-    namespace: "editor",
-    theme: ExampleTheme,
-    // Handling of errors during update
-    onError(error: any) {
+  const initialConfig = {
+    namespace: 'nexusgraph',
+    nodes: [...EditorNodes],
+    onError: (error: Error) => {
       throw error;
     },
-    // Any custom nodes go here
-    nodes: [
-      HeadingNode,
-      ListNode,
-      ListItemNode,
-      QuoteNode,
-      TableNode,
-      TableCellNode,
-      TableRowNode,
-      AutoLinkNode,
-      LinkNode,
-      CodeNode,
-      CodeHighlightNode,
-    ],
+    theme: EditorTheme,
   };
 
   return (
-    <LexicalComposer initialConfig={editorConfig}>
-      <div className={styles["editor-container"]}>
-        <ToolbarPlugin />
-        <Paragraph>
-          <RichTextPlugin
-            contentEditable={<ContentEditable className={styles["editor-input"]} />}
-            placeholder={<Placeholder />}
-            ErrorBoundary={LexicalErrorBoundary}
-          />
-          <HistoryPlugin />
-          <AutoFocusPlugin />
-          <CheckListPlugin />
-          <CodeHighlightPlugin />
-          <OnChangePlugin />
-          <EditorContentUpdatePlugin />
-          <ListPlugin />
-          <LinkPlugin />
-        </Paragraph>
-      </div>
+    <LexicalComposer initialConfig={initialConfig}>
+      <SharedHistoryContext>
+        <TableContext>
+          <SharedAutocompleteContext>
+            {/* <header>
+              <a href="https://lexical.dev" target="_blank" rel="noreferrer">
+                <img src={logo} alt="Lexical Logo" />
+              </a>
+            </header> */}
+            <div className="editor-shell">
+              <Editor />
+            </div>
+            <Settings />
+            {/* {isDevPlayground ? <DocsPlugin /> : null}
+            {isDevPlayground ? <PasteLogPlugin /> : null}
+            {isDevPlayground ? <TestRecorderPlugin /> : null}
+
+            {measureTypingPerf ? <TypingPerfPlugin /> : null} */}
+          </SharedAutocompleteContext>
+        </TableContext>
+      </SharedHistoryContext>
     </LexicalComposer>
   );
-}
-
-function Placeholder(): JSX.Element {
-  return <div className={styles["editor-placeholder"]}>请输入文本......</div>;
 }
