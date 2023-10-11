76f42f33b36af3bab14920028fdfdd98b0b54303
gongminghui <xxykx1016@163.com>
2023-10-11 14:06:54 +0800

    Login refactoring (#190)
    * Login refactoring
    
    * Temporarily disable sign-in in prod and add it back in next PR

--- packages/nexusgraph-app/src/ProdApp.tsx
+++ packages/nexusgraph-app/src/ProdApp.tsx
@@ -1,51 +1,21 @@
 // Copyright 2023 Paion Data. All rights reserved.
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { useLogto } from "@logto/react";
-import * as Sentry from "@sentry/react";
-
+import { Callback, useOAuth2 } from "../../nexusgraph-oauth2";
 import App from "./App";
-import { Callback } from "./LogtoProvider";
-
-export default function ProdApp(): JSX.Element {
-  if (process.env.SENTRY_IO_DSN) {
-    setupSentry();
-  }
-
-  const { signIn, isAuthenticated } = useLogto();
-
-  if (isAuthenticated) {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/login" element={<Callback />} />
-        </Routes>
-      </Router>
-    );
-  }
-
-  if (process.env.NODE_ENV == "production") {
-    signIn("https://app.nexusgraph.com/login");
-  }
-
-  return <></>; // this line never reaches
-}
 
 /**
- * Connects to Nexus Graph's monitoring system
+ * The {@link ProdApp} involves OAuth2 authentication and authorization.
+ *
+ * @returns
  */
-function setupSentry(): void {
-  Sentry.init({
-    dsn: process.env.SENTRY_IO_DSN as string,
-    integrations: [
-      new Sentry.BrowserTracing({
-        tracePropagationTargets: [/^https:\/\/app\.nexusgraph\.com/],
-      }),
-      new Sentry.Replay(),
-    ],
-    tracesSampleRate: 1.0,
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-  });
+export default function ProdApp(): JSX.Element {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App oauthContext={useOAuth2()} />} />
+        <Route path="/login" element={<Callback />} />
+      </Routes>
+    </Router>
+  );
 }
