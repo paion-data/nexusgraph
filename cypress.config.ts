fd2b95bf305bcac105a65a0ee34821530ff3a7be
gongminghui <xxykx1016@163.com>
2023-10-11 14:06:52 +0800

    implement login (#142)

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,12 +2,20 @@
  * Copyright 2023 Paion Data. All rights reserved.
  */
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    supportFile: false,
+    supportFile: "cypress/support/commands.ts",
+    env: {
+      username: process.env.TEST_USER_NAME as string,
+      password: process.env.TEST_USER_PASSWORD as string,
+      logtoEndpointUrl: process.env.LOGTO_ENDPOINT_URL as string,
+    },
   },
 });
