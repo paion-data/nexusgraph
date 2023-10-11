2fb6a288eb9c934f7b5efb8d9e8bed6fdee55f13
gongminghui <xxykx1016@163.com>
2023-10-11 14:06:51 +0800

    nexusgraph basic test (#120)
    Signed-off-by: gongminghui <xxykx1016@163.com>

---
+++ cypress/e2e/nexusgraph.spec.cy.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2023 Paion Data. All rights reserved.
+ */
+describe("nexusgraph basic test", () => {
+  it("Enter text in the editor and the corresponding node is generated in the graph", () => {
+    cy.visit("http://localhost:8080/", { failOnStatusCode: false }).wait(1000);
+
+    cy.intercept("GET", "/entityExtraction?sentence=China", { fixture: "getEditorData.json" }).as("getEditorData");
+
+    cy.get(".editor-paragraph").type("China").wait(1000);
+    cy.get(".node").should("contain", "China").should("have.length", 6);
+  });
+
+  it('Click the menu "Expand" button to expand the neighbor node', () => {
+    cy.visit("http://localhost:8080/", { failOnStatusCode: false });
+
+    cy.intercept("GET", "/entityExtraction?sentence=China", { fixture: "getEditorData.json" }).as("getEditorData");
+
+    cy.get(".editor-paragraph").type("China").wait(1000);
+
+    cy.get('[aria-label="graph-nodeBeijing"]').click();
+
+    cy.intercept("GET", "/expand?node=%7B%22fields%22:%7B%22name%22:%22Beijing%22%7D,%22id%22:%22Beijing%22%7D", {
+      fixture: "nodeExpandData.json",
+    }).as("nodeExpandData");
+
+    cy.get('g[class="node selected"] path:first').click().wait(1000);
+
+    cy.get(".node").should("contain", "Shanghai");
+  });
+});
+
+export {};
---
+++ scripts/wait-on-config.js
@@ -0,0 +1,4 @@
+// Copyright 2023 Paion Data. All rights reserved.
+module.exports = {
+  timeout: 60000,
+};
