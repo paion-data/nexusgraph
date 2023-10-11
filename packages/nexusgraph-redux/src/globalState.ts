dc6a68b7059e81597e12c0cbc2c610464afdfb80
gongminghui <xxykx1016@163.com>
2023-10-11 14:06:54 +0800

    Implement note list duck (#201)
    * Implement notes title directory duck
    
    * Add doc
    
    * Rename noteListDuck
    
    * Code Style

--- packages/nexusgraph-redux/src/globalState.ts
+++ packages/nexusgraph-redux/src/globalState.ts
@@ -1,5 +1,6 @@
 // Copyright 2023 Paion Data. All rights reserved.
 import { NlpState, NLP_DATA as nlpData } from "./nlp/nlpTypes";
+import { NoteInfo, NOTE_LIST_STATE as noteList } from "./note-list/noteListTypes";
 import { NoteState, NOTE_STATE as note } from "./note/noteTypes";
 import { OAuthState, OAUTH_STATE as oAuth } from "./oAuth/oAuthType";
 
@@ -7,4 +8,5 @@ export interface GlobalState {
   [nlpData]: NlpState;
   [note]: NoteState;
   [oAuth]: OAuthState;
+  [noteList]: NoteInfo[];
 }
--- packages/nexusgraph-redux/src/rootReducer.ts
+++ packages/nexusgraph-redux/src/rootReducer.ts
@@ -1,6 +1,8 @@
 // Copyright 2023 Paion Data. All rights reserved.
 import nlpReducer from "./nlp/nlpDuck";
 import { NLP_DATA as nlpData } from "./nlp/nlpTypes";
+import noteListReducer from "./note-list/noteListDuck";
+import { NOTE_LIST_STATE as noteList } from "./note-list/noteListTypes";
 import noteReducer from "./note/noteDuck";
 import { NOTE_STATE as note } from "./note/noteTypes";
 import oAuthReducer from "./oAuth/oAuthDuck";
@@ -10,4 +12,5 @@ export default {
   [nlpData]: nlpReducer,
   [note]: noteReducer,
   [oAuth]: oAuthReducer,
+  [noteList]: noteListReducer,
 };
