9a8edc13f75d51b10a96a41d76ea9333111580ba
gongminghui <xxykx1016@163.com>
2023-10-11 14:06:52 +0800

    Fix the login redirect issue in production (#154)
    * Fix the login redirect issue in production
    
    * Update HC deployment accordingly
    
    * Self-review
    
    * Self-review
    
    * Self-review
    
    * Self-review
    
    * Fix test error
    
    * Style

--- hashicorp/scripts/setup-app.sh
+++ hashicorp/scripts/setup-app.sh
@@ -15,6 +15,9 @@ sudo apt install -y nodejs
 sudo npm install -g npm
 sudo npm install -g yarn
 
+# Install serve for http server running app
+sudo npm install -g serve
+
 # Bundle up app
 git clone https://github.com/paion-data/nexusgraph.git
 cd /home/ubuntu/nexusgraph
@@ -27,13 +30,3 @@ sudo apt install -y nginx
 sudo mv /home/ubuntu/nginx-ssl.conf /etc/nginx/sites-enabled/default
 sudo mv /home/ubuntu/server.crt /etc/ssl/certs/server.crt
 sudo mv /home/ubuntu/server.key /etc/ssl/private/server.key
-
-# Install Python 3.10 for http server running app
-sudo apt update && sudo apt upgrade -y
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt install python3.10 -y
-sudo apt install python3-pip -y
-sudo apt install python3.10-venv -y
-alias python=python3.10
-alias python3=python3.10
