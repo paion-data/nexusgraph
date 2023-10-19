f01d564f63796f1cc34542df7f8dbb940dd603e6
gongminghui <xxykx1016@163.com>
2023-10-19 12:11:02 +0800

    E2E test

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       password: process.env.TEST_USER_PASSWORD as string,
       logtoEndpointUrl: process.env.LOGTO_ENDPOINT_URL as string,
       nodeEnv: process.env.NODE_ENV as string,
-      entityExtractionServer: (process.env.THERESA_API_URL as string) + "entityExtraction/",
+      entityExtractionServer: (process.env.THERESA_API_URL as string) + "entityExtraction",
       nodeExpand: (process.env.THERESA_API_URL as string) + "expand/",
       astraiosGraphqlEndpoint: process.env.ASTRAIOS_API_RESOURCE as string,
     },
--- cypress/e2e/alert.spec.cy.ts
+++ cypress/e2e/alert.spec.cy.ts
@@ -5,19 +5,22 @@ beforeEach(() => {
 
   const intelligentAIButton = cy.get(".modal-content>div>div>button");
   intelligentAIButton.click();
+
+  cy.get("textarea").type("今天是个好日子");
+  const createGraphButton = cy.get(".modalContent>div>div>button");
+  createGraphButton.click();
 });
 
 it("Displays the alert when nlp data is recognized as null", () => {
-  const createGraphButton = cy.get(".modalContent>div>button");
-  createGraphButton.click();
+  cy.intercept("POST", Cypress.env("astraiosGraphqlEndpoint"), {
+    body: { links: [], nodes: [] },
+  });
 
   cy.get("div[id='alert']").should("exist");
 });
 
 it("Alert is not displayed when nlp data is recognized as not null", () => {
-  cy.get("textarea").type("今天是个好日子");
-  const createGraphButton = cy.get(".modalContent>div>button");
-  createGraphButton.click();
+  cy.intercept("POST", Cypress.env("astraiosGraphqlEndpoint"), { fixture: "getNlpRespond.json" });
 
   cy.get("div[id='alert']").should("not.empty");
 });
