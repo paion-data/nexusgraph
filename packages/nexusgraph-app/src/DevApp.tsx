8c20dbaa925441a6956885672c7e8530eb853099
Jack <jack20220723@gmail.com>
2023-10-31 18:17:10 +0800

    bind for dev app

--- packages/nexusgraph-app/src/DevApp.tsx
+++ packages/nexusgraph-app/src/DevApp.tsx
@@ -2,16 +2,21 @@
 import { useDispatch } from "react-redux";
 import { OAuthState, updateOAuthState } from "../../nexusgraph-redux";
 import App from "./App";
+import { TYPES, container } from "../inversify.config";
 
 export default function DevApp(): JSX.Element {
   const dispatch = useDispatch();
 
-  const devOAuthState: OAuthState = {
-    accessToken: "devToken",
-    userInfo: { sub: "devUserId" },
-  };
+  const devToken = "devToken"
+  const devUserId = "devUserId"
+  const devUserInfo = { sub: devUserId };
 
-  dispatch(updateOAuthState(devOAuthState));
+  dispatch(updateOAuthState({
+    accessToken: devToken,
+    userInfo: devUserInfo,
+  }));
+  container.bind<string>(TYPES.accessToken).toConstantValue(devToken)
+  container.bind<string>(TYPES.userId).toConstantValue(devUserId)
 
   return <App />;
 }
