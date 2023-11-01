f61a83a225fa3ee443ab1f30fbfc4a3e0a5fe6b0
Jack <jack20220723@gmail.com>
2023-11-01 19:19:04 +0800

    update

--- packages/nexusgraph-astraios/src/AstraiosClient.ts
+++ packages/nexusgraph-astraios/src/AstraiosClient.ts
@@ -1,6 +1,5 @@
 // Copyright 2023 Paion Data. All rights reserved.
 import { GraphName, GraphState } from "../../nexusgraph-redux";
-import { NoteInfo } from "../../nexusgraph-redux/src/note-list/noteListDuck";
 
 /**
  * This interface is used to define the data format for sending requests to the backend storage server Astraios
