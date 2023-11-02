3bf3b94260bd635c4d1d0434e3820a5da44b0528
Jack <jack20220723@gmail.com>
2023-11-02 12:04:55 +0800

    Refactoring NLP state to Graph state (#247)
    * Refactoring NLP state to Graph state
    
    * Fix compile error
    
    * Fix compile error

--- packages/nexusgraph-redux/src/rootReducer.ts
+++ packages/nexusgraph-redux/src/rootReducer.ts
@@ -1,11 +1,11 @@
 // Copyright 2023 Paion Data. All rights reserved.
-import nlpReducer, { NLP_DATA as nlpData } from "./nlp/nlpDuck";
+import graphReducer, { GRAPH_DATA as graphData } from "./graph/graphDuck";
 import noteListReducer, { NOTE_LIST_STATE as noteList } from "./note-list/noteListDuck";
 import noteReducer, { NOTE_STATE as note } from "./note/noteDuck";
 import oAuthReducer, { OAUTH_STATE as oAuth } from "./oAuth/oAuthDuck";
 
 export default {
-  [nlpData]: nlpReducer,
+  [graphData]: graphReducer,
   [note]: noteReducer,
   [oAuth]: oAuthReducer,
   [noteList]: noteListReducer,
