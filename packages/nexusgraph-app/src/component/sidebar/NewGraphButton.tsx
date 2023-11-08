138c8bc8807f3d2aa3d4206c682dcf876bd85f07
Jack <jack20220723@gmail.com>
2023-11-08 10:07:44 +0800

    Renaming button for creating a new graph

--- packages/nexusgraph-app/src/component/sidebar/NewGraphButton.tsx
+++ packages/nexusgraph-app/src/component/sidebar/NewGraphButton.tsx
@@ -1,23 +1,23 @@
 // Copyright 2023 Paion Data. All rights reserved.
 import { PlusIcon as PlusIconSolid } from "@heroicons/react/24/solid";
 import { FeatureMenu } from "./FeatureMenu";
-import { CreateButtonStyled } from "./styled";
+import { StyledNewGraphButton } from "./styled";
 import useModal from "./useModal";
 
-export default function CreateButton({ setShowAlert }: any): JSX.Element {
+export default function NewGraphButton({ setShowAlert }: any): JSX.Element {
   const PlusIcon = (): JSX.Element => <PlusIconSolid />;
   const [modal, showModal] = useModal(false);
 
   return (
     <>
-      <CreateButtonStyled
+      <StyledNewGraphButton
         id="createButton"
         onClick={() => {
           showModal((onClose) => <FeatureMenu onClose={onClose} setShowAlert={setShowAlert} />);
         }}
       >
         <PlusIcon />
-      </CreateButtonStyled>
+      </StyledNewGraphButton>
       {modal}
     </>
   );
