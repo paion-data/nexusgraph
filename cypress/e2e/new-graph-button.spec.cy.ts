ec98ae14e9983168568507019f40eebbe6bb8ee1
Jack <jack20220723@gmail.com>
2023-11-10 19:12:36 +0800

    Merge E2E tests for 'new-graph button' (#290)

--- cypress/e2e/new-graph-button.spec.cy.ts
+++ cypress/e2e/new-graph-button.spec.cy.ts
@@ -16,4 +16,26 @@ describe("New Graph button guides user to create graph with different options", 
         expect(cy.wrap($div).children("div").children("button")).to.exist;
       });
   });
+
+  it("shows up a graph by some generating texts", () => {
+    const NLPMethodButton = cy.get("button[id='newGraphMethodButton-NLP']");
+    NLPMethodButton.click();
+
+    cy.get("textarea").type("我爱中国");
+
+    const createGraphButton = cy.get(".modal-content>div>div>div>button");
+    createGraphButton.click();
+
+    cy.intercept("POST", Cypress.env("entityExtractionServer"), { fixture: "single-rdf-tuple-from-theresa.json" });
+
+    cy.get("div[id='alert']").should("not.empty");
+
+    cy.get("svg")
+      .find(`[aria-label^="graph-node"]`)
+      .then(($el) => {
+        expect($el[0].textContent).contains(" 我");
+        expect($el[1].textContent).contains(" 中国");
+      });
+    cy.get("svg").find('[class="relationship"]').contains("爱");
+  });
 });
