ab7e71652c81c1a9a36ba42fdaad6865752b4808
Jack <jack20220723@gmail.com>
2023-11-17 16:55:15 +0800

    Update test

--- cypress/e2e/graph-title.spec.cy.ts
+++ cypress/e2e/graph-title.spec.cy.ts
@@ -16,11 +16,10 @@ describe("Graph title", () => {
 
   it("When user update title, new title is reflected in both sidebar list as well as after being refreshed", () => {
     cy.get('[data-testid="graphTitle"]').click({ force: true }).clear().type("Modified Title");
-    cy.get("body").click(0, 0); // off-clicking the input box
-
-    cy.reload(); // making sure the edit gets persisted
 
     cy.get('[data-testid="graphTitle"]').click({ force: true }).should("have.value", "Modified Title");
-    cy.get('[data-testid^="graphListItem-"]').contains("Modified Title");
+
+    cy.reload();
+    cy.get('[data-testid^="graphListItem-"]').contains("Modified Title"); // making sure the edit gets persisted
   });
 });
