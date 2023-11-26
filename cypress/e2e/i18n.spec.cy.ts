5d78889487cb1c27523de1eb5dfd43fef23062f6
Jack <jack20220723@gmail.com>
2023-11-26 14:11:53 +0800

    Style

--- cypress/e2e/i18n.spec.cy.ts
+++ cypress/e2e/i18n.spec.cy.ts
@@ -2,16 +2,12 @@
 describe("i18n", () => {
   it("supports English", () => {
     cy.setBrowserLanguage("en-US", ["en"], ["en"]);
-    cy.newGraph()
-      .get('[data-testid="vizInspector"]')
-      .contains("Node labels");
+    cy.newGraph().get('[data-testid="vizInspector"]').contains("Node labels");
   });
 
   it("supports Chinese", () => {
     cy.setBrowserLanguage("zh-CN", ["zh"], ["zh"]);
 
-    cy.newGraph()
-      .get('[data-testid="vizInspector"]')
-      .contains("节点类型");
+    cy.newGraph().get('[data-testid="vizInspector"]').contains("节点类型");
   });
 });
