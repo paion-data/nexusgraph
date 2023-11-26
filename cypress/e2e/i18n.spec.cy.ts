358bacc9d92cb3a25ea5482ba0961bb69acb5289
Jack <jack20220723@gmail.com>
2023-11-26 12:27:17 +0800

    Update tests

--- cypress/e2e/i18n.spec.cy.ts
+++ cypress/e2e/i18n.spec.cy.ts
@@ -1,14 +1,8 @@
 // Copyright 2023 Paion Data. All rights reserved.
 describe("i18n", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000/");
-  });
-
   it("supports English", () => {
     cy.setBrowserLanguage("en-US", ["en"], ["en"]);
     cy.newGraph()
-      .get("input[data-testid=graphTitle]")
-      .should("have.value", "Unamed Graph")
       .get('[data-testid="vizInspector"]')
       .contains("Node labels");
   });
@@ -17,8 +11,6 @@ describe("i18n", () => {
     cy.setBrowserLanguage("zh-CN", ["zh"], ["zh"]);
 
     cy.newGraph()
-      .get("input[data-testid=graphTitle]")
-      .should("have.value", "无标题笔记")
       .get('[data-testid="vizInspector"]')
       .contains("节点类型");
   });
