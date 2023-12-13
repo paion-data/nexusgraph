9f4a8694224d67f9197e9906489aeed80de6ceb2
Jack <jack20220723@gmail.com>
2023-12-13 19:11:11 +0800

    Add Argos test

--- cypress/e2e/screenshot.spec.cy.ts
+++ cypress/e2e/screenshot.spec.cy.ts
@@ -3,8 +3,10 @@
 import "@argos-ci/cypress/support";
 
 describe("Argos tests", () => {
-  it("screenshot homepage", async ({ page: any }) => {
+  const pages = [{ name: "homepage", path: "/" }];
+
+  it("screenshot homepage", async () => {
     cy.visit("http://localhost:3000/");
-    cy.argosScreenshot("/");
+    cy.argosScreenshot("homepage");
   });
 });
