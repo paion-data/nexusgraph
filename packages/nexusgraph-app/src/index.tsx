a623b0d15d24e8191192e1a8d8e7fb9b3fbf6cdb
Jiaqi Liu ｜【烬火】胡桃 <jack20220723@gmail.com>
2025-01-26 19:54:37 +0800

    Patch sentry monitoring config (#3)

--- packages/nexusgraph-app/src/index.tsx
+++ packages/nexusgraph-app/src/index.tsx
@@ -1,11 +1,45 @@
+/**
+ * Copyright 2025 Jiaqi Liu. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Provider } from "react-redux";
 import App from "./App";
-import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+import * as Sentry from "@sentry/react";
+import { store } from "nexusgraph-redux";
+
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://431084c0bdf0ec7a71a0283db266469a@o4505480921022464.ingest.us.sentry.io/4508709263572992",
+    integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ["localhost", /^https:\/\/nexusgraph\.qubitpi\.org/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
